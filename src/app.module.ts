f80e4d467257f489f7389dc02ff5106d82670fa4
0afcode <eagl3007@gmail.com>
2025-09-04 15:29:04 -0400

    plug in user module, modularize config for typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,30 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { DbTestService } from './dbtest.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSourceOptions } from 'typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'admin',
-      password: 'leetsauce',
-      database: 'appdb',
-      entities: [],
-      synchronize: false,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['src/env/.env.development'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: 'mysql',
+          host: configService.get('MARIADB_HOST'),
+          port: configService.get<Number>('MARIADB_PORT'),
+          username: configService.get('MARIADB_USER'),
+          password: configService.get('MARIADB_PASS'),
+          database: configService.get('MARIADB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          migrations: ['dist/migrations/*.js'],
+          synchronize: configService.get<Boolean>('DB_SYNC') || false,
+        }) as DataSourceOptions,
     }),
     UserModule,
   ],
